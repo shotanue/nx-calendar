529c96cb75683a07bc2a586f93ef0844eb839653
shotanue <22065594+shotanue@users.noreply.github.com>
2020-03-01 23:45:07 +0900

    updating tailwindcss for using grid. create calendar ui(mock)

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,90 @@
 <template>
-  <div class="text-center bg-blue-100">
-    <span class="text-black">hello world</span>
+  <div class="flex h-screen">
+    <div class="w-0 lg:w-2/6 bg bg-orange-700">
+      todo
+    </div>
+    <main class="h-screen w-screen lg:w-4/6 bg bg-purple-700 text-white text-center">
+      <header class="container mx-auto grid grid-cols-3 h-16 content-center" style="height: 10vh">
+        <div class="flex-1 opacity-50">
+          <span class="block">February</span>
+          <span class="block">2020</span>
+        </div>
+        <div class="flex-1">
+          <span class="block">March</span>
+          <span class="block">2020</span>
+        </div>
+        <div class="flex-1 opacity-50">
+          <span class="block">April</span>
+          <span class="block">2020</span>
+        </div>
+      </header>
+      <div class="container mx-auto grid grid-cols-7" style="height: 5vh">
+        <div class="font-bold">
+          Sun
+        </div>
+        <div class="font-bold">
+          Mon
+        </div>
+        <div class="font-bold">
+          Tue
+        </div>
+        <div class="font-bold">
+          Wed
+        </div>
+        <div class="font-bold">
+          Thu
+        </div>
+        <div class="font-bold">
+          Fri
+        </div>
+        <div class="font-bold">
+          Sat
+        </div>
+      </div>
+      <div class="container mx-auto grid grid-cols-7" style="height: 85vh">
+        <span class="opacity-50">23</span>
+        <span class="opacity-50">24</span>
+        <span class="opacity-50">25</span>
+        <span class="opacity-50">26</span>
+        <span class="opacity-50">27</span>
+        <span class="opacity-50">28</span>
+        <span class="opacity-50">29</span>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+        <span>11</span>
+        <span>12</span>
+        <span>13</span>
+        <span>14</span>
+        <span>15</span>
+        <span>16</span>
+        <span>17</span>
+        <span>18</span>
+        <span>19</span>
+        <span>20</span>
+        <span>21</span>
+        <span>22</span>
+        <span>23</span>
+        <span>24</span>
+        <span>25</span>
+        <span>26</span>
+        <span>27</span>
+        <span>28</span>
+        <span>29</span>
+        <span>30</span>
+        <span>31</span>
+        <span class="opacity-50">1</span>
+        <span class="opacity-50">2</span>
+        <span class="opacity-50">3</span>
+        <span class="opacity-50">4</span>
+      </div>
+    </main>
   </div>
 </template>
-
-<script>
-export default {
-  components: {
-  }
-}
-</script>
